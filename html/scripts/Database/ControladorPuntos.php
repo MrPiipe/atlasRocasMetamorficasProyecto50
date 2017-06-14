a35246734bd1e4a2c02b8f45cf74522965c12ae6
Proyecto50 <proyecto50@localhost.localdomain>
2017-06-14 12:47:58 -0500

    adding maps

--- html/scripts/Database/ControladorPuntos.php
+++ html/scripts/Database/ControladorPuntos.php
@@ -1,38 +1,38 @@
 <?php
-    //TODO: Implementar REDIS
-    /*
-    $bd = "atlasmetamorfico";
-    $server ="localhost";
-    $user = "root";
-    $password = "";
+//TODO: Implementar REDIS
+/*
+$bd = "atlasmetamorfico";
+$server ="localhost";
+$user = "root";
+$password = "";
 
-    $conexion = @mysqli_connect($server, $user, $password, $bd);
+$conexion = @mysqli_connect($server, $user, $password, $bd);
 
-    if (! $conexion) {
-        die("Error de conexion ".mysqli_connect_error());
-    }
+if (! $conexion) {
+    die("Error de conexion ".mysqli_connect_error());
+}
 
-    $sql = "SELECT * FROM ATLAS_MAPA2";
-    $result = mysqli_query($conexion, $sql);
-    $array_user = array();
-    while ($data = mysqli_fetch_assoc($result)) {
-        $array_user[] = $data;
-    }
+$sql = "SELECT * FROM ATLAS_MAPA2";
+$result = mysqli_query($conexion, $sql);
+$array_user = array();
+while ($data = mysqli_fetch_assoc($result)) {
+    $array_user[] = $data;
+}
 
-    echo json_encode($array_user);
-    */
-    require "../predis/autoload.php";
-    Predis\Autoloader::register();
+echo json_encode($array_user);
+*/
+require "../predis/autoload.php";
+Predis\Autoloader::register();
 
-    $redis   = new Predis\Client();
+$redis   = new Predis\Client();
 
-    $ids = $redis->Lrange('ItemIds', 0, -1);
-    $mapArray = array();
-    foreach ($ids as $id) {
-        $mapArray['name'][$i] = $redis->Lrange('ItemNames', $i, $i);
-        $mapArray['ids'][$i]  = $redis->Lrange('ItemIds', $i, $i);
-        $mapArray['longitude'][$i] = $redis->Lrange('longitude', $i, $i);
-        $mapArray['latitude'][$i] = $redis->Lrange('Latitude', $i, $i);
-        $mapArray['zone'][$i] = $redis->Lrange('Zone', $i, $i);
-    }
-    echo json_encode($mapArray);
+$ids = $redis->Lrange('itemIds', 0, -1);
+$mapArray = array();
+for ($i = 0; $i < count($ids);$i++) {
+    $mapArray['name'][$i] = $redis->Lrange('itemNames', $i, $i);
+    $mapArray['codigo'][$i]  = $redis->Lrange('itemIds', $i, $i);
+    $mapArray['longitud'][$i] = $redis->Lrange('Longitudes', $i, $i);
+    $mapArray['latitud'][$i] = $redis->Lrange('Latitudes', $i, $i);
+    $mapArray['zona'][$i] = $redis->Lrange('Zones', $i, $i);
+}
+echo json_encode($mapArray);
